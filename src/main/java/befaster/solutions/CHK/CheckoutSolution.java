fdb7d1080e1e7149086cc4c9069cc2760a1bebc0
sbx_user1051 <sbx_user1051@169.254.23.17>
2024-05-18 16:12:46 +0000

    Sat May 18 16:12:46 UTC 2024

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -8,43 +8,37 @@ public class CheckoutSolution {
         int nr_b=0;
         int nr_c=0;
         int nr_d=0;
+        if (skus.isEmpty())
+            return -1;
         for (char i:skus.toCharArray())
         {
-            if (i=='A')
-            {
-                nr_a++;
+
+                if (i=='A')
+                {
+                    nr_a++;
+                }
+                else if (i=='B')
+                    nr_b++;
+                else if (i=='C')
+                    nr_c++;
+                else nr_d++;
             }
-            else if (i=='B')
-                nr_b++;
-            else if (i=='C')
-                nr_c++;
-            else nr_d++;
+
+        if (nr_a==0||nr_b==0||nr_c==0||nr_d==0)
+        {
+            return -1;
         }
         int sum=0;
-        if (nr_a%3==0)
-        {
-            sum+=nr_a/3*130;
-        }
-        else if ((nr_a>3)&&(nr_a%3!=0))
-        {
-            sum+=nr_a/3*130;
-            sum+=nr_a%3*50;
-        }
 
-        if (nr_b%2==0)
-        {
-            sum+=nr_b/2*45;
-        }
-        else if ((nr_b>2)&&(nr_b%2!=0))
-        {
-            sum+=nr_b/2*45;
-            sum+=nr_b%2*30;
-        }
-
+        sum+=nr_a/3*130;
+        sum+=nr_a%3*50;
+        sum+=nr_b/2*45;
+        sum+=nr_b%2*30;
         sum+=nr_c*20;
         sum+=nr_d*15;
         return sum;
 
     }
 }
+
 
